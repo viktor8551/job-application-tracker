3cf4a4bad342f1454aaceb0c72df2ffe619b9d5f
viktor8551 <viktor.walser@live.com>
2026-05-31 08:57:18 +0200

    feat: add postgres setup

---
+++ api/Data/AppDbContext.cs
@@ -0,0 +1,9 @@
+using api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
+}
--- api/Program.cs
+++ api/Program.cs
@@ -1,6 +1,12 @@
+using api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
